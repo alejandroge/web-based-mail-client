06854d3e089dc21b723fdabe849f85984239ea26
Alejandro Guevara <alexg@auctionedge.com>
2021-01-01 21:16:36 -0600

    Make the App start on the Mail Inbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root "emails#index"
+
   resources :emails, except: :update
+
   get 'hello/say_hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
